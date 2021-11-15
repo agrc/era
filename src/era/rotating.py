fc1c2ae6bd68456ce0df4bae4030ec665e46f81a
Jake Adams <jdadams@utah.gov>
2021-11-15 16:36:57 -0700

    chore: streamline folder rotation

--- src/era/rotating.py
+++ src/era/rotating.py
@@ -8,17 +8,14 @@ module_logger = logging.getLogger(__name__)
 
 class FolderRotator:
 
-    def __init__(self, base_dir, prefix='') -> None:
+    def __init__(self, base_dir) -> None:
         self.base_dir = base_dir
-        self.prefix = prefix
-        self.pattern = f'{self.prefix}[0-9]{{8}}_[0-9]{{6}}'
-
         self._class_logger = logging.getLogger(__name__).getChild(self.__class__.__name__)
 
     #: Make dir Methods
-    def _get_new_download_dir_path(self, date_format):
+    def _get_new_download_dir_path(self, prefix, date_format):
         today = datetime.today()
-        download_dir = self.base_dir / f'{self.prefix}{today.strftime(date_format)}'
+        download_dir = self.base_dir / f'{prefix}{today.strftime(date_format)}'
         return download_dir
 
     def _make_new_download_dir(self, download_dir_path, exist_ok):
@@ -31,16 +28,10 @@ class FolderRotator:
             self._class_logger.debug(f'Successfully created {download_dir_path}')
             return download_dir_path
 
-    def create_download_dir(self, date_format='%Y%m%d_%H%M%S', exist_ok=False):
-        download_dir = self._get_new_download_dir_path(date_format)
-        created_dir_path = self._make_new_download_dir(download_dir, exist_ok)
-        return created_dir_path
-
     #: Rotator Methods
-    def _get_all_but_n_most_recent_folder_paths(self, custom_pattern=None, max_folder_count=10):
-        if custom_pattern:
-            self.pattern = custom_pattern
-        folder_paths = [path for path in self.base_dir.iterdir() if re.match(self.pattern, path.stem)]
+    def _get_all_but_n_most_recent_folder_paths(self, prefix, pattern, max_folder_count):
+        pattern = f'{prefix}{pattern}'
+        folder_paths = [path for path in self.base_dir.iterdir() if re.match(pattern, path.stem)]
         return sorted(folder_paths)[-max_folder_count:]
 
     def _delete_old_folders(self, folder_paths_to_delete):
@@ -57,8 +48,13 @@ class FolderRotator:
 
         return deleted_folders
 
-    def rotate_download_dirs(self, custom_pattern=None, max_folder_count=10):
-        folder_paths_to_delete = self._get_all_but_n_most_recent_folder_paths(custom_pattern, max_folder_count)
+    def get_download_dir(
+        self, prefix='', date_format='%Y%m%d_%H%M%S', exist_ok=False, pattern='[0-9]{8}_[0-9]{6}', max_folder_count=10
+    ):
+        download_dir = self._get_new_download_dir_path(prefix, date_format)
+        created_dir_path = self._make_new_download_dir(download_dir, exist_ok)
+        folder_paths_to_delete = self._get_all_but_n_most_recent_folder_paths(prefix, pattern, max_folder_count)
         deleted_folders = self._delete_old_folders(folder_paths_to_delete)
+        self._class_logger.info(f'Deleted folder(s) for rotation: {deleted_folders}')
 
-        return deleted_folders
+        return created_dir_path
